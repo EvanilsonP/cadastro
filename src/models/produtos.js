37afcf1003fd995ac4262f987b3cf83afe9aae2c
EvanilsonP <pevanilson99@gmail.com>
2022-12-09 10:44:11 -0300

    Configurando model

---
+++ src/models/produtos.js
@@ -0,0 +1,31 @@
+const db = require('../database');
+const { DataTypes } = require('sequelize');
+
+// Cria a estrutura/Model
+const Produtos = db.define("Produtos", { // Nome do model, especificar quais colunas existem na tabela e configurar TABLENAME
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    preco: {
+        type: DataTypes.FLOAT
+    },
+
+    quantidade: {
+        type: DataTypes.INTEGER
+    },
+
+    createdAt: {
+        type: DataTypes.DATE
+    },
+
+    updatedAt: {
+        type: DataTypes.DATE
+    },
+}, {
+    tableName: 'produtos' // Especificar nome da tabela. Por padrão, o sequelize vai pensar que minha tabela se chama 'PRODUCTS'.
+});
+
+module.exports = Produtos;
